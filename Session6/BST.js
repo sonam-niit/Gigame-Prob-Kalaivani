9fb8cc6e71b82befa833540d6c72c43738652b86
sonam-niit <sonam.gravity@gmail.com>
2024-04-15 22:07:10 +0530

    BST

--- Session6/BST.js
+++ Session6/BST.js
@@ -32,6 +32,59 @@ class BinarySearchTree{
             }
         }
     }
+    delete(data){
+        this.deleteNode(this.root,data)
+    }
+    deleteNode(node,data){
+        if(node===null){
+            return null; //nothing to delete
+        }
+        if(data<node.key){
+            node.left=this.deleteNode(node.left,data);
+            return node;
+        }else if(data>node.key){
+            node.right=this.deleteNode(node.right,data);
+            return node;
+        }else{
+            //Node is detected for deletion
+            //node is the leaf node
+            if(node.left===null && node.right===null){
+                node=null;
+                return node;
+            }
+            //node is having one child
+            if(node.left===null){
+                node=node.right;
+                return node;
+            }else if(node.right===null){
+                node=node.left;
+                return node;
+            }
+            //Node is having both child
+            const minRightNode=this.findMinNode(node.right);
+            node.key=minRightNode.key;
+            node.right=this.deleteNode(node.right,minRightNode.key);
+            return node;
+        }
+    }
+    findMinNode(node){
+        while(node.left!==null){
+            node=node.left;
+        }
+        return node;
+    }
+    inOrderTraversalToArray(){
+        const result=[];
+        this.inOrderTraversalToArrayRecursive(this.root,result);
+        return result;
+    }
+    inOrderTraversalToArrayRecursive(node,result){
+        if(node!=null){
+            this.inOrderTraversalToArrayRecursive(node.left,result);
+            result.push(node.key);
+            this.inOrderTraversalToArrayRecursive(node.right,result);
+        }
+    }
 }
 
 const myBST= new BinarySearchTree();
@@ -39,4 +92,4 @@ const arr=[8,3,10,1,6,14,4,7,13];
 for(let i of arr){
     myBST.insert(i);
 }
-console.log(myBST);
+console.log(myBST.inOrderTraversalToArray());
